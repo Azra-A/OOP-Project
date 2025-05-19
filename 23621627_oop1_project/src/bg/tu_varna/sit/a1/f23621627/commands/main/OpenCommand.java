851aebf1dfcd918950b83a47f15af8b08a4eead5
Azra-A <aazra276@gmail.com>
2025-05-19 22:13:05 +0300

    Update OpenCommand.java

--- 23621627_oop1_project/src/bg/tu_varna/sit/a1/f23621627/commands/main/OpenCommand.java
+++ 23621627_oop1_project/src/bg/tu_varna/sit/a1/f23621627/commands/main/OpenCommand.java
@@ -3,6 +3,9 @@ package bg.tu_varna.sit.a1.f23621627.commands.main;
 import bg.tu_varna.sit.a1.f23621627.commands.Command;
 import bg.tu_varna.sit.a1.f23621627.core.FileManager;
 
+/**
+ * Executes the command to open a file using the FileManager.
+ */
 public class OpenCommand implements Command {
     private final FileManager fileManager;
 
@@ -10,6 +13,11 @@ public class OpenCommand implements Command {
         this.fileManager = fileManager;
     }
 
+    /**
+     * Opens a file specified by the arguments.
+     *
+     * @param arguments the file path to open
+     */
     @Override
     public void execute(String arguments) {
         fileManager.open(arguments);
