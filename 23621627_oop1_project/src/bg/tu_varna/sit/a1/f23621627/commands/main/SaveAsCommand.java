504f2c1956de1630300a894833882ebe93e46686
Azra-A <aazra276@gmail.com>
2025-05-19 22:13:33 +0300

    Update SaveAsCommand.java

--- 23621627_oop1_project/src/bg/tu_varna/sit/a1/f23621627/commands/main/SaveAsCommand.java
+++ 23621627_oop1_project/src/bg/tu_varna/sit/a1/f23621627/commands/main/SaveAsCommand.java
@@ -3,6 +3,9 @@ package bg.tu_varna.sit.a1.f23621627.commands.main;
 import bg.tu_varna.sit.a1.f23621627.commands.Command;
 import bg.tu_varna.sit.a1.f23621627.core.FileManager;
 
+/**
+ * Command to save the currently opened file to a new location.
+ */
 public class SaveAsCommand implements Command {
     private final FileManager fileManager;
 
@@ -10,6 +13,12 @@ public class SaveAsCommand implements Command {
         this.fileManager = fileManager;
     }
 
+    /**
+     * Saves the current file content to a new file specified by the argument.
+     * Prints messages if no file is open or no path is provided.
+     *
+     * @param arguments the new file path to save as
+     */
     @Override
     public void execute(String arguments) {
         if (!fileManager.isFileOpen()) {
@@ -25,4 +34,3 @@ public class SaveAsCommand implements Command {
         fileManager.saveAs(arguments.trim());
     }
 }
-
