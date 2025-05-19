87ffba8e9e52ff704d4e000293c9d677a0434e71
Azra-A <aazra276@gmail.com>
2025-05-19 22:11:44 +0300

    Update CloseCommand.java

--- 23621627_oop1_project/src/bg/tu_varna/sit/a1/f23621627/commands/main/CloseCommand.java
+++ 23621627_oop1_project/src/bg/tu_varna/sit/a1/f23621627/commands/main/CloseCommand.java
@@ -3,13 +3,27 @@ package bg.tu_varna.sit.a1.f23621627.commands.main;
 import bg.tu_varna.sit.a1.f23621627.commands.Command;
 import bg.tu_varna.sit.a1.f23621627.core.FileManager;
 
+/**
+ * Command that closes the currently opened file.
+ */
 public class CloseCommand implements Command {
+
     private final FileManager fileManager;
 
+    /**
+     * Creates a new CloseCommand.
+     *
+     * @param fileManager used to manage file operations
+     */
     public CloseCommand(FileManager fileManager) {
         this.fileManager = fileManager;
     }
 
+    /**
+     * Executes the close command.
+     *
+     * @param arguments not used
+     */
     @Override
     public void execute(String arguments) {
         fileManager.close();
