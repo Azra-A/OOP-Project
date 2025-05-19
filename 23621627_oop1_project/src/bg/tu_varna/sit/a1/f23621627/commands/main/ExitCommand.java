99a71a40720fbfb64ff3912bf0e740a274b34874
Azra-A <aazra276@gmail.com>
2025-05-19 22:12:14 +0300

    Update ExitCommand.java

--- 23621627_oop1_project/src/bg/tu_varna/sit/a1/f23621627/commands/main/ExitCommand.java
+++ 23621627_oop1_project/src/bg/tu_varna/sit/a1/f23621627/commands/main/ExitCommand.java
@@ -2,11 +2,19 @@ package bg.tu_varna.sit.a1.f23621627.commands.main;
 
 import bg.tu_varna.sit.a1.f23621627.commands.Command;
 
+/**
+ * Command that exits the program.
+ */
 public class ExitCommand implements Command {
+
+    /**
+     * Executes the exit command.
+     *
+     * @param arguments not used
+     */
     @Override
     public void execute(String arguments) {
         System.out.println("Exiting the program...");
         System.exit(0);
     }
 }
-
