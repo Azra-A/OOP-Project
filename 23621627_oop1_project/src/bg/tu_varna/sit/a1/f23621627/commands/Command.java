47eb4e91496d14b86e7b439659c394880d9594e0
Azra-A <aazra276@gmail.com>
2025-05-19 22:06:07 +0300

    Update Command.java

--- 23621627_oop1_project/src/bg/tu_varna/sit/a1/f23621627/commands/Command.java
+++ 23621627_oop1_project/src/bg/tu_varna/sit/a1/f23621627/commands/Command.java
@@ -1,5 +1,13 @@
 package bg.tu_varna.sit.a1.f23621627.commands;
 
+/**
+ * Represents a command that can be executed.
+ */
 public interface Command {
-    public void execute(String arguments);
+    /**
+     * Executes the command.
+     *
+     * @param arguments command arguments, may be null or empty
+     */
+    void execute(String arguments);
 }
